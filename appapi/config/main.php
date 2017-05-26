14c167e17ba5a083d2ffe004eaf26482e973647f
phperv5 <312043814@qq.com>
2017-05-26 16:31:34 +0800

    first commit

---
+++ appapi/config/main.php
@@ -0,0 +1,52 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'appapi\controllers',
+	'modules'=>$modules,
+	//'bootstrap' => ['log'],
+	//'bootstrap'    => ['assetsAutoCompress'],
+	# 自定义组件
+    'components' => [
+       /*
+	   'assetsAutoCompress' =>
+        [
+            'class'             => '\skeeks\yii2\assetsAuto\AssetsAutoCompressComponent',
+            'enabled'           => true,
+            'jsCompress'        => true,
+            'cssFileCompile'    => true,
+            'jsFileCompile'     => true,
+        ],
+		*/
+		'session' => [
+			/**
+			 * use mongodb for session.
+			 */
+			/*
+			'class' => 'yii\mongodb\Session',
+			'db' => 'mongodb',
+			'sessionCollection' => 'session',
+			*/
+			'class' => 'yii\redis\Session',
+			'timeout' => 6000,
+		],
+		
+		'cache' => [
+			/**
+			 * use mongodb for cache.
+			 */
+			//'class' => 'yii\mongodb\Cache',
+            'class' => 'yii\redis\Cache',
+            'keyPrefix' => 'appapi',
+        ],
+	],
+	# 自定义参数
+    'params' => $params,
+];
---
+++ apphtml5/config/YiiClassMap.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	//'fecshop\app\apphtml5\helper\test\My' => '@apphtml5/helper/My.php',   
+	
+];
